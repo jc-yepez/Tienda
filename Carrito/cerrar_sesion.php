49299331a970bcf989e42a6c5d12aca3ebfb9cf9
jc-yepez <104331598+jc-yepez@users.noreply.github.com>
2022-04-25 18:21:12 -0500

    Delete Carrito directory

--- Carrito/cerrar_sesion.php
+++
@@ -1,6 +0,0 @@
-<?php 
-session_start();
-unset($_SESSION['ncar']);
-session_destroy();
-header("Location: index.html");
-?>
--- Carrito/setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server="localhost";
-$db="carrito";
-$user="root";
-$pass="";
-$version="0.9";
-$pgport=8080;
-$pchartfolder="./class/pchart2";
